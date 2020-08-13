cfd132a90683c1318e31710d0e880a9d30a77485
Kevin Alwell <alwell-kevin@github.com>
2020-08-13 16:12:00 -0400

    Update routes.js

--- api/routes.js
+++ api/routes.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
   var arithmetic = require("./controllers/arithmeticController");
-
+  var myToken = "8256ba5a40014ed318f8f22cc15227b579e341ef";
   app.route("/arithmetic").get(arithmetic.calculate);
 };
